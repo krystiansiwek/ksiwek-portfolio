4f4c2a00ac1538a0538ac3c4b3712f0957f66295
krystiansiwek <krystian.siwek@yahoo.com>
2020-07-10 14:26:27 +0200

    add custom 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <div class="error">
+      <h1 class="error__name">404</h1>
+      <h2 class="error__small-header">nothing to see here</h2>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "404",
+  },
+};
+</script>
+
+<style scoped>
+.error {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+}
+
+.error__name,
+.error__small-header {
+  margin: 0;
+  color: #ffffff;
+  font-weight: lighter;
+  text-align: center;
+}
+</style>
